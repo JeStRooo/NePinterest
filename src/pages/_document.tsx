75f8a0e03cc90a220c1f3f46404074a0914bb665
JeStRooo <euroset270578@gmail.com>
2023-10-15 23:52:06 +0300

    master - create next app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
